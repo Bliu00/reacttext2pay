8caf2ca03dc6fbd4c048ecde44e4b80aca5bef71
julialiu16 <julia.zihan.liu@gmail.com>
2020-01-21 16:28:12 -0800

    yayyy payment ui works

--- src/components/PayCard.jsx
+++ src/components/PayCard.jsx
@@ -1,65 +1,8 @@
 import React, {Component} from 'react';
+import Button from './button';
+import getConfig from './config';
 import Woodstocks from "../images/Woodstock.jpeg"
 class PayCard extends Component{
-  onBuyClicked() {
-    if (!window.PaymentRequest) {
-      // PaymentRequest API is not available. Forwarding to
-      // legacy form based experience.
-      //location.href = '/checkout';
-      //return;
-    }
-  
-    // Supported payment methods
-    var supportedInstruments = [{
-        supportedMethods: ['basic-card'],
-        data: {
-          supportedNetworks: [
-            'visa', 'mastercard', 'amex', 'discover',
-            'diners', 'jcb', 'unionpay'
-          ]
-        }
-    }];
-  
-    var val = 24.50;//REPLACE WITH AMOUNT FROM GETPAYMENT REQ
-  
-    // Checkout details
-    var details = {
-      total: {
-        label: 'Total due',
-        amount: { currency: 'USD', value : val }
-      }
-    };
-  
-    // 1. Create a `PaymentRequest` instance
-    var request = new PaymentRequest(supportedInstruments, details);
-  
-    // 2. Show the native UI with `.show()`
-    request.show()
-    // 3. Process the payment
-    .then(result => {
-      console.log('result is: ' , result)
-      // POST the payment information to the server
-      return fetch('/pay', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({...result.toJSON(),...{amountBase:val}})
-      }).then(response => {
-        // 4. Display payment results
-        if (response.status === 200) {
-          // Payment successful
-          return result.complete('success');
-        } else {
-          // Payment failure
-          return result.complete('fail');
-        }
-      }).catch(() => {
-        return result.complete('fail');
-      });
-    });
-  }
   render(){
     return(
       <div className="card" style={{width:"450px",height:"525px", marginTop:"40px",display: "inline-block"}}>
@@ -75,7 +18,7 @@ class PayCard extends Component{
           <a id="decline" href="home" className="btn btn-light" style = {{ width:"300px",height:"60px",fontSize:"16px",paddingTop: "18px",marginTop: "10px"}} > No Thanks</a>
 
           <br /> 
-          <a id="pay" href="#" className="btn btn-info" style = {{ width:"300px",height:"60px",fontSize: "16px", paddingTop: "18px",marginTop: "10px"}} onclick="fadeButton()" > Pay Now</a>
+          <Button config={getConfig()} style = {{ width:"300px",height:"60px",fontSize: "16px", paddingTop: "18px",marginTop: "10px"}} > Pay Now</Button>
           <br />
           <p id="report" className="card-text" style = {{fontSize: "14px",textAlign: "center",marginTop: "15px"}}>Think there was a mistake?  
             <a href="#" className="link" style = {{width:"250px",height:"35px",fontSize:"14px"}}> Report your error
@@ -89,5 +32,4 @@ class PayCard extends Component{
   }
   
 }
-
 export default PayCard;
---
+++ src/components/config.tsx
@@ -0,0 +1,97 @@
+  
+import { PaymentRequestParams } from 'react-payment-request-api';
+
+const details: PaymentDetailsInit = {
+  displayItems: [{
+    label: 'Original donation amount',
+    amount: { currency: 'USD', value: '65.00' },
+  }, {
+    label: 'Friends and family discount',
+    amount: { currency: 'USD', value: '-10.00' },
+  }, {
+    label: 'Delivery tax',
+    pending: true,
+    amount: { currency: 'USD', value: '10.00' },
+  }],
+  total: {
+    label: 'Total due',
+    amount: { currency: 'USD', value : '55.00' },
+  },
+};
+
+const getConfig = (supportedPaymentCards: string[], onShowSuccess: () => void) => ({
+  methodData: [
+    {
+      supportedMethods: ['basic-card'],
+      data: {
+        supportedNetworks: ['visa', 'mastercard', 'diners'],
+      },
+    },
+    {
+      supportedMethods: ['https://android.com/pay'],
+      data: {
+        merchantId: 'fake',
+        environment: 'TEST',
+        allowedCardNetwork: ['AMEX', 'MASTERCARD', 'VISA', 'DISCOVER'],
+        paymentMethodTokenizationParameters: {
+          tokenizationType: 'GATEWAY_TOKEN',
+          parameters: {
+            'gateway': 'stripe',
+            'stripe:publishableKey': 'fake',
+            'stripe:version': '2016-07-06'
+          },
+        },
+      },
+    },
+    {
+      supportedMethods: 'https://apple.com/apple-pay',
+      data: {
+          version: 3,
+          merchantIdentifier: 'merchant.com.example',
+          merchantCapabilities: ['supportsDebit'],
+          supportedNetworks: ['masterCard', 'visa'],
+          countryCode: 'US',
+      },
+    },
+  ],
+  details: details,
+  options: {
+    requestShipping: true,
+    requestPayerEmail: true,
+    requestPayerPhone: true,
+  },
+  onShowSuccess: (result, resolve, reject): void => {
+    /* tslint:disable-next-line:no-console */
+    console.log('Result:', result);
+    // make the payment
+    setTimeout(() => { onShowSuccess(); resolve(); }, 2000);
+  },
+  /* tslint:disable-next-line:no-console */
+  onShowFail: (error) => console.log('Error', error),
+  onShippingAddressChange: (request, resolve, reject): void => {
+    /* tslint:disable-next-line:no-console */
+    console.log('ShippingAddress:', request.shippingAddress);
+    // recalculate details
+    details.shippingOptions = [{
+      id: 'all',
+      label: 'Wherever you want for free',
+      amount: { currency: 'USD', value: '0.00' },
+      selected: true
+    }];
+    details.displayItems![2] = {
+      label: 'Tax',
+      pending: false,
+      amount: { currency: 'USD', value: '8.00' },
+    };
+    resolve(details);
+  },
+  onShippingOptionChange: (request, resolve, reject): void => {
+    resolve(details);
+  },
+  // tslint:disable-next-line:no-any
+  onMerchantValidation: (event: any): void => {
+    event.complete(Promise.resolve(event.validationURL));
+  },
+}) as PaymentRequestParams;
+
+export default getConfig;
